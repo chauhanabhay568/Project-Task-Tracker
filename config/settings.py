44d75491ee1c64491af23877a794c90d816f2db8
Abhay <chauhanabhay568@gmail.com>
2026-09-05 00:46:02 +0530

    fix CSRF trusted origins for Railway

--- config/settings.py
+++ config/settings.py
@@ -27,6 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-fxyx@yfx8wml0_2g@um#s
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+CSRF_TRUSTED_ORIGINS = [f'https://{h}' for h in ALLOWED_HOSTS if h not in ('localhost', '127.0.0.1')]
 
 
 # Application definition
